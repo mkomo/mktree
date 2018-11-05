87d9fad8494ce251411a3ec97b2c8f69d8f39a8a
Matt Komorowski <matt@mkomo.com>
2018-11-05 12:44:55 -0500

    Add mouseover and group click.

--- mktree.js
+++ mktree.js
@@ -175,19 +175,29 @@ class MkTreeView {
       filters: props.filters || {},
       filtersOn: [],
       searchTerms: props.searchTerms || null,
-      focusNode: props.focusNode || props.root
+      focusNodes: props.focusNodes || [props.root]
     }
 
+    let view = this;
     this.svg = d3.select("body").append("svg")
       .attr("id", 'graph_svg')
+    let tooltip = d3.select("body").append("span")
+      .attr("id", 'tooltip');
+    this.tooltip = tooltip;
+    d3.select("body").on('mousemove', function(){
+      tooltip
+        .style("left", (d3.event.pageX + 28) + "px")		
+        .style("top", (d3.event.pageY + 28) + "px");	
+    })
+    this.svg.on('mouseover', d=>{view.hover()});
     this.container = this.svg.append('g');
 
     let sideBar = d3.select("body").append("div")
       .attr("id", 'info_box');
 
-    this.searchBox = sideBar.append('input').attr('class', 'info_element');
+    this.searchBox = sideBar.append('input').attr('class', 'info_element')
+      .attr('placeholder', 'search graph');
     this.infoBox = sideBar.append('table').attr('class', 'info_element');
-    let view = this;
     this.searchBox.on('keypress', function(e){
       if (!e) e = window.event;
       var keyCode = e.keyCode || e.which;
@@ -198,6 +208,7 @@ class MkTreeView {
     })
 
     this.search = this.search.bind(this);
+    this.hover = this.hover.bind(this);
   }
 
   setState(newState) {
@@ -218,18 +229,20 @@ class MkTreeView {
     debug('search!!!', criteria);
     //search
     let term = criteria.toLowerCase();
+    let nodes = [];
     root.dft(function(node){
       // search
       for (let key in node.state.data) {
         if ((this.state.searchTerms == null || this.state.searchTerms.includes(key)) && node.state.data[key].toLowerCase().includes(term)) {
-          debug('found',node.state.data[key],node);
-          this.setState({
-            //root: node,
-            focusNode: node
-          })
+          //debug('found',node.state.data[key],node);
+          nodes.push(node);
+          return;
         }
       }
     }, {}, this);
+    this.setState({
+      focusNodes: nodes
+    })
   }
   draw(changed) {
 
@@ -261,7 +274,7 @@ class MkTreeView {
   }
   */
   levelPath(dimensions, margin, level, levelNodes, maxLevel) {
-    let w = dimensions.width - margin.l - margin.r;
+    let w = dimensions.graphWidth - margin.l - margin.r;
     let h = dimensions.height - margin.t - margin.b;
     let frac = (1 - 3/(level + 3));
     let maxFrac = (1 - 3/(maxLevel + 3));
@@ -295,6 +308,18 @@ class MkTreeView {
           angle: Math.atan(1/m)*180/Math.PI
         }
       }
+    }
+  }
+  
+  hover(node) {
+    d3.event.stopPropagation();
+    if (node) {
+      this.tooltip
+        .style('display', 'inline-block')
+        .text(node.state.data.Name);
+    } else {
+      this.tooltip
+        .style('display', 'none')      
     }
   }
 
@@ -331,7 +356,8 @@ class MkTreeView {
         }
         totalbreadth += 2 * nodes[node.state.id].r;
       });
-      if (totalbreadth > levelPath.length + 100) {
+      //TODO handle case where level 1 does not have enough spread (there's no way to see all those nodes ever)
+      if (level > 1 && totalbreadth > levelPath.length + 100) {
         let lastParent;
         let lastColor;
         let lastNonHidden;
@@ -417,10 +443,24 @@ class MkTreeView {
 
     groupEdges.enter().append("polygon")
       .attr("class", 'group_edge')
+      .classed('clickable', true)
       .style('fill', d=>{
         return view.state.colorFunction(d[1]);
       })
       .style('opacity',0)
+      .on('mouseover', function(d) {
+        view.hover(d[0].node);
+      })
+      .on('click', function(d) {
+        view.setState({
+          focusNodes: [d[0].node]
+        })
+      })
+      .on('dblclick', function(d) {
+        view.setState({
+          root: d[0].node
+        })
+      })
       .transition(t)
           .delay(500)
         .style('opacity',0.5)
@@ -450,10 +490,10 @@ class MkTreeView {
         return !node.isHidden && !('span' in node);
       }), d => d.node.state.id);
 
-    debug('start circle with focusNode', view.state.focusNode);
+    debug('start circle with focusNodes', view.state.focusNodes);
     circle.exit().remove();
     circle
-      .classed('focus', d=>(d.node == view.state.focusNode))
+      .classed('focus', d=>(view.state.focusNodes.includes(d.node)))
       .transition(t)
           .attr("x", function(d) {
             return d.x - d.r;
@@ -471,10 +511,14 @@ class MkTreeView {
     
     circle.enter().append("rect")
       .classed('node', true)
-      .classed('focus', d=>(d.node == view.state.focusNode))
+      .classed('clickable', true)
+      .classed('focus', d=>(view.state.focusNodes.includes(d.node)))
+      .on('mouseover', function(d) {
+        view.hover(d.node);
+      })
       .on('click', function(d) {
         view.setState({
-          focusNode: d.node
+          focusNodes: [d.node]
         })
       })
       .on('dblclick', function(d) {
@@ -513,8 +557,22 @@ class MkTreeView {
 
     groupNodes.enter().append("polygon")
       .attr("class", 'group_node')
+      .classed('clickable', true)
       .style('fill', view.state.colorFunction)
       .style('opacity',0)
+      .on('mouseover', function(d) {
+        view.hover(d.node.getParent());
+      })
+      .on('click', function(d) {
+        view.setState({
+          focusNodes: [d.node.getParent()]
+        })
+      })
+      .on('dblclick', function(d) {
+        view.setState({
+          root: d.node.getParent()
+        })
+      })
       .transition(t)
           .delay(500)
         .style('opacity',1)
@@ -540,9 +598,13 @@ class MkTreeView {
 
     nodelabels.enter().append("text")
       .attr("class", 'nodelabel')
+      .classed('clickable', true)
+      .on('mouseover', function(d) {
+        view.hover(d.node);
+      })
       .on('click', function(d) {
         view.setState({
-          focusNode: d.node
+          focusNodes: [d.node]
         })
       })
       .on('dblclick', function(d) {
@@ -608,41 +670,47 @@ class MkTreeView {
   updateFocus() {
     let view = this;
     this.container.selectAll("rect")
-      .classed('focus', d=> (d.node == view.state.focusNode));
+      .classed('focus', d=> (view.state.focusNodes.includes(d.node)));
     debug('start infoBox');
-    if (this.state.focusNode) {
-      let graphInfo = {
-        nodeCount: 0,
-        levelCount: 0,
-      }
-
-      let info = this.state.infoBox(this.state.focusNode);
-      var entries = this.infoBox.selectAll("tr").data(Object.keys(info));
-
-      entries.exit().remove();
-
-      let rows = entries.enter().append("tr")
-        .attr("class", 'datum')
-      .merge(entries)
-      entries = rows.selectAll('td')
-        .data(function (row) {
-            return [[row, 'label'], [info[row]]]
-        })
-      entries
-        .enter()
-            .append('td')
-        .merge(entries)
-            .text(d => d[0])
-            .attr('class', d=>(d.length > 1 ? d[1] : null))
+    let info;
+    if (this.state.focusNodes.length > 1) {
+      info = {};
+      this.state.focusNodes.forEach((node, i) => {
+        let f = this.state.infoBox(node);
+        info[i] = Object.values(f).join('; ')
+      });
+    } else if (this.state.focusNodes.length == 1) {
+      info = this.state.infoBox(this.state.focusNodes[0]);
     } else {
-      this.infoBox.html('');
+      info = {
+        nodeCount: this.state.root.getRoot().getNodeCount(true),
+        levelCount: this.state.root.getRoot().getMaxDepth()
+      }
     }
+    var entries = this.infoBox.selectAll("tr").data(Object.keys(info));
+
+    entries.exit().remove();
+
+    let rows = entries.enter().append("tr")
+      .attr("class", 'datum')
+    .merge(entries)
+    entries = rows.selectAll('td')
+      .data(function (row) {
+          return [[row, 'label'], [info[row]]]
+      })
+    entries
+      .enter()
+          .append('td')
+      .merge(entries)
+          .text(d => d[0])
+          .attr('class', d=>(d.length > 1 ? d[1] : null))
     debug('end infoBox')
   }
 
   getDimensions() {
     return {
       width: window.innerWidth,
+      graphWidth: window.innerWidth * 0.66,
       height: window.innerHeight -
         (document.getElementById('graph_svg').getBoundingClientRect().top -
           document.body.getBoundingClientRect().top)
